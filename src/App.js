160f79151d21615f4c946d45cdfe78e9babe7894
DivyaBharathi8 <divyabharathi0807@gmail.com>
2025-09-01 12:33:00 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
+import Header from "./header";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
